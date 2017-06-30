8d722264f618c26c9ccead9305f16bdc97578177
p.del.conte@hotmail.ch <rick@DESKTOP-VTKI94C.localdomain>
2017-07-01 01:58:16 +0200

    basic grid functionality

---
+++ EpicGameJam2017/Assets/Scripts/Hexagon/HexagonCell.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework.Constraints;
+using UnityEngine.Assertions;
+
+[RequireComponent(typeof(MeshRenderer), typeof(MeshFilter))]
+public class HexagonCell : MonoBehaviour
+{
+    public const float outer2inner = 0.866025404f; //srt(3)/2
+    public const float outerRadius = 3f;
+    public const float innerRadius = outerRadius * outer2inner;
+
+    public Color Color = Color.blue;
+    public bool Outline = true;
+
+    private Mesh mesh;
+    private List<Vector3> vertices = new List<Vector3>();
+    private List<int> triangles = new List<int>();
+
+    private static readonly Vector3[] corners =
+    {
+        new Vector3(0, outerRadius),
+        new Vector3(outerRadius, outerRadius * 0.5f),
+        new Vector3(outerRadius, outerRadius * -0.5f),
+        new Vector3(0, -outerRadius),
+        new Vector3(-outerRadius, outerRadius * -0.5f),
+        new Vector3(-outerRadius, outerRadius * 0.5f),
+        //repeat first corner for lazy bum drawing
+        new Vector3(0, outerRadius),
+    };
+
+    void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh = new Mesh();
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Triangulate();
+        GetComponent<MeshRenderer>().material.color = Color;
+        //TODO
+        if (Outline)
+        {
+
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void setPosition(float x, float y)
+    {
+        transform.position = new Vector3(x, y);
+    }
+
+    private void Triangulate()
+    {
+        //triangulate hexagon
+        var center = transform.localPosition;
+        for (int i = 0; i < 6; i++)
+        {
+            AddTriangle(
+                center,
+                center + corners[i],
+                center + corners[i + 1]
+            );
+        }
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals();
+    }
+
+    void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
+    {
+        int vertexIndex = vertices.Count;
+        vertices.Add(v1);
+        vertices.Add(v2);
+        vertices.Add(v3);
+        triangles.Add(vertexIndex);
+        triangles.Add(vertexIndex + 1);
+        triangles.Add(vertexIndex + 2);
+    }
+}
---
+++ EpicGameJam2017/Assets/Scripts/Hexagon/HexagonGrid.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class HexagonGrid : MonoBehaviour
+{
+
+    public int Height = 10;
+    public int Width = 10;
+    public HexagonCell HexagonCell;
+    private GameObject hexcellGameObject;
+
+    void Awake()
+    {
+        hexcellGameObject = HexagonCell.gameObject;
+    }
+
+    // Use this for initialization
+    void Start () {
+        CreateGrid(Width,Height);
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
+
+    void CreateGrid(int width, int height)
+    {
+        var cellwidth = HexagonCell.outerRadius;
+        var cellheight = HexagonCell.innerRadius * 1.5f / 2;
+
+        var ncols = (int)(width / cellwidth)+1;
+        var nrows = (int)(height / cellheight)+1;
+
+        //center is at 0/0 => tiles start at  - ncols / 2 * cellwidth
+  
+        var offsetX = -ncols * cellwidth / 2f + cellwidth/2f;
+        var offsetY = -nrows / 2f * cellheight + cellheight/2f; 
+
+
+        List<GameObject> hexagons = new List<GameObject>(ncols*nrows);
+        for (var i = 0; i < nrows; i++)
+        {
+            for (var j = 0; j < ncols; j++)
+            {
+                var x = offsetX + i * cellwidth + (j%2)*cellwidth/2f;
+                var y = offsetY + j * cellheight;
+            
+                //place hexagon at x,y
+                var hexcell = Instantiate(hexcellGameObject);
+                hexcell.GetComponent<HexagonCell>().setPosition(x,y);
+                hexagons.Add(hexcell);
+            }
+        }
+        
+
+    }
+}
