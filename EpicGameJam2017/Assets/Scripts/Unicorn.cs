cc6785428feeeffbf522e474958ead028ce384c8
TheJP <jp_55555@hotmail.com>
2017-07-01 01:38:01 +0200

    Created simple unicorn control script

--- EpicGameJam2017/Assets/Scripts/Unicorn.cs
+++ EpicGameJam2017/Assets/Scripts/Unicorn.cs
@@ -3,9 +3,34 @@ using UnityEngine;
 
 public class Unicorn : MonoBehaviour
 {
+    [Tooltip("Player which controls this unicorn")]
     public Players player;
+
+    [Tooltip("Wheter the unicorn is currently switched on or not (can be changed by the player with the Switch key)")]
+    public bool controlsActive = false;
+
+    [Tooltip("Unicorn turnspeed in degrees per second")]
+    public float turnSpeed = 360f;
 
     public void Update()
     {
+        if (controlsActive)
+        {
+            // Turn unicorn
+            if(Input.GetButton(Constants.LeftButton + player))
+            {
+                transform.Rotate(0f, 0f, turnSpeed * Time.deltaTime);
+            }
+            if(Input.GetButton(Constants.RightButton + player))
+            {
+                transform.Rotate(0f, 0f, -turnSpeed * Time.deltaTime);
+            }
+        }
+
+        // Switch between unicorn and train
+        if(Input.GetButtonDown(Constants.SwitchButton + player))
+        {
+            controlsActive = !controlsActive;
+        }
     }
 }
