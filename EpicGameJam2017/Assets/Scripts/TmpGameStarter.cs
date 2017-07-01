b1eb925ef153a235cfc1d307c26b1d77a1810145
TheJP <jp_55555@hotmail.com>
2017-07-01 15:17:13 +0200

    Started work on main scene

---
+++ EpicGameJam2017/Assets/Scripts/TmpGameStarter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TmpGameStarter : MonoBehaviour
+{
+    void Start()
+    {
+        FindObjectOfType<Controller>().StartGame(new[] { Players.A, Players.B });
+    }
+}
