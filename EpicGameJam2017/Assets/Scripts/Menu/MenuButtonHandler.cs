0193e579282d3790606c319c6fffc3569afe77d7
Stefan Winterberger <stefanwinterb88@hotmail.com>
2017-07-01 18:03:31 +0200

    Added Draft of MainMenu

---
+++ EpicGameJam2017/Assets/Scripts/Menu/MenuButtonHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonHandler : MonoBehaviour {
+
+    public void loadSceneByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void loadSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+	
+}
