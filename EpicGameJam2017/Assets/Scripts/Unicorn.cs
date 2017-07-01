70cc6ed36bc8cab409ed9791d017b16d00ff5d15
TheJP <jp_55555@hotmail.com>
2017-07-01 03:28:21 +0200

    Improved unicorn steering

--- EpicGameJam2017/Assets/Scripts/Unicorn.cs
+++ EpicGameJam2017/Assets/Scripts/Unicorn.cs
@@ -10,11 +10,14 @@ public class Unicorn : MonoBehaviour
     [Tooltip("Wheter the unicorn is currently switched on or not (can be changed by the player with the Switch key)")]
     public bool controlsActive = false;
 
-    [Tooltip("Unicorn turnspeed (torque force modifier)")]
+    [Tooltip("Unicorn turnspeed in degrees per second")]
     public float turnSpeed = 1f;
 
     [Tooltip("Force which is added when the action key is pressed")]
     public float speedForce = 10f;
+
+    [Tooltip("How much the unicorn dirfts")]
+    public float driftFactor = 0.25f;
 
     public void Update()
     {
@@ -30,13 +33,28 @@ public class Unicorn : MonoBehaviour
         if (controlsActive)
         {
             var body = GetComponent<Rigidbody2D>();
+
+            body.velocity = ForwardVelocity() + driftFactor * RightVelocity();
+
             // Turn unicorn
-            body.AddTorque(Input.GetAxis(Constants.HorizontalAxis + player) * turnSpeed, ForceMode2D.Force);
+            body.angularVelocity = Input.GetAxis(Constants.HorizontalAxis + player) * turnSpeed;
             // Accelerate unicorn
             if (Input.GetButton(Constants.ActionButton + player))
             {
                 body.AddForce(transform.up * speedForce, ForceMode2D.Force);
             }
         }
+    }
+
+    /// <summary>Forward part of the velocity</summary>
+    private Vector2 ForwardVelocity()
+    {
+        return transform.up * Vector2.Dot(GetComponent<Rigidbody2D>().velocity, transform.up);
+    }
+
+    /// <summary>Forward part of the velocity</summary>
+    private Vector2 RightVelocity()
+    {
+        return transform.right * Vector2.Dot(GetComponent<Rigidbody2D>().velocity, transform.right);
     }
 }
