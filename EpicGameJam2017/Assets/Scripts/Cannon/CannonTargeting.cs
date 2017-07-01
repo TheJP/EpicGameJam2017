ab24336eed8c8e39631157c45a6f81dfb0df3854
Florian Graf <florian.graf@gmx.ch>
2017-07-01 13:59:42 +0200

    Added particle effects for damaged turrets

--- EpicGameJam2017/Assets/Scripts/Cannon/CannonTargeting.cs
+++ EpicGameJam2017/Assets/Scripts/Cannon/CannonTargeting.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
 using UnityEngine;
 
 public class CannonTargeting : MonoBehaviour
@@ -7,11 +9,7 @@ public class CannonTargeting : MonoBehaviour
   [Tooltip("The shell that will be copied and fired")]
   public GameObject shell;
 
-  [Tooltip("The time it takes to repair a turret")]
-  public float repairTime = 5.0f;
-
   private bool isBroken;
-  private float repairTimeLeft;
   private bool isFiringAllowed;
   private bool isFiring;
   private float firingDistance;
@@ -26,11 +24,7 @@ public class CannonTargeting : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
   {
-    if(repairTimeLeft > 0)
-    {
-      repairTimeLeft -= Time.deltaTime;
-    }
-    else if(isFiringAllowed && Input.GetButton(Constants.ActionButton + cannon.player))
+    if(isFiringAllowed && !isBroken && Input.GetButton(Constants.ActionButton + cannon.player))
     {
       // The button is being pressed, increase the distance we will fire
       isFiring = true;
@@ -73,7 +67,15 @@ public class CannonTargeting : MonoBehaviour
 
   public void Break()
   {
-    repairTimeLeft = repairTime;
+    isBroken = true;
     isFiring = false;
+
+    transform.position -= -transform.right * firingDistance;
+    firingDistance = 0;
+  }
+
+  public void Repair()
+  {
+    isBroken = false;
   }
 }
--- EpicGameJam2017/Assets/Scripts/Cannon/TurretDamage.cs
+++ EpicGameJam2017/Assets/Scripts/Cannon/TurretDamage.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class TurretDamage : MonoBehaviour
 {
+  [Tooltip("The time it takes to repair a turret")]
+  public float repairTime = 5.0f;
+
+  private float repairTimeLeft;
   private CannonTargeting cannonTargeting;
+  private ParticleSystem damageParticles;
 
 	// Use this for initialization
-	void Start ()
+	void Start()
 	{
 	  cannonTargeting = GetComponentInChildren<CannonTargeting>();
+	  damageParticles = GetComponentInChildren<ParticleSystem>();
+    damageParticles.gameObject.SetActive(false);
 	}
+
+  void Update()
+  {
+    repairTimeLeft -= Time.deltaTime;
+
+    if(repairTimeLeft <= 0)
+    {
+      repairTimeLeft = 0;
+      cannonTargeting.Repair();
+      damageParticles.gameObject.SetActive(false);
+    }
+  }
 	
   void OnTriggerEnter2D(Collider2D other)
   {
+    damageParticles.gameObject.SetActive(true);
     cannonTargeting.Break();
 
     Destroy(other.gameObject);
+
+    repairTimeLeft = repairTime;
   }
 }
