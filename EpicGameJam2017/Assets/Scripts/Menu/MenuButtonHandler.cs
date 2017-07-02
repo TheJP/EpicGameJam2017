20d2d0c2c8497439c5c8a74cde22fc4179697dee
Stefan Winterberger <stefanwinterb88@hotmail.com>
2017-07-02 13:26:55 +0200

    Modified Menu

--- EpicGameJam2017/Assets/Scripts/Menu/MenuButtonHandler.cs
+++ EpicGameJam2017/Assets/Scripts/Menu/MenuButtonHandler.cs
@@ -6,12 +6,20 @@ using UnityEngine.SceneManagement;
 public class MenuButtonHandler : MonoBehaviour {
 
     public MenuController menuController;
+    public UserInformations userInformations;
+
 
     public void startGame()
     {
-        menuController.StartNewGame();
+        int numberOfPlayers = menuController.getPlayers().Count;
+        if ( numberOfPlayers == 0)
+        {
+            userInformations.Alert("Register Player First!", 4L);
+        } else
+        {
+            menuController.StartNewGame();
+        }
     }
-
 
     public void quitOnClick()
     {
