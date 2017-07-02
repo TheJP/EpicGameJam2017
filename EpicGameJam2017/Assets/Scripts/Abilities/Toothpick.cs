0bb388e723cd6ecf8db4defa5fee0fec04c86d7b
TheJP <jp_55555@hotmail.com>
2017-07-02 13:12:43 +0200

    Improved toothpick (now despawns at world border and damages turrets)

--- EpicGameJam2017/Assets/Scripts/Abilities/Toothpick.cs
+++ EpicGameJam2017/Assets/Scripts/Abilities/Toothpick.cs
@@ -17,20 +17,38 @@ public class Toothpick : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Collision with other ingredients or with the bowl kills the toothpick
         var ingredient = collision.gameObject.GetComponent<Ingredient>() != null;
         var provider = collision.gameObject.GetComponent<IngredientProvider>() != null;
         if (ingredient || provider)
         {
-            // Collision with other ingredients or with the bowl kills the toothpick
             Destroy(gameObject);
         }
+
+        // Don't damage the unicorn that casted the toothpick
         if (collision.gameObject == Thrower.gameObject) { return; }
+
+        // Stun unicorns if hit
         var unicorn = collision.gameObject.GetComponent<Unicorn>();
-        if(unicorn != null)
+        if (unicorn != null)
         {
             unicorn.Stun();
             Destroy(gameObject);
 
+        }
+
+        // Damage turret if hit
+        var turret = collision.gameObject.GetComponent<TurretDamage>();
+        if (turret != null)
+        {
+            turret.Damage();
+            Destroy(gameObject);
+        }
+
+        // Remove toothpick if border was reached
+        if (collision.gameObject.tag == "WorldBorder")
+        {
+            Destroy(gameObject);
         }
     }
 }
