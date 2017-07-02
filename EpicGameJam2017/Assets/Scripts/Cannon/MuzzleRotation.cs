ecaf40afb99e00a2039a93a2510c1530ec462e3b
Florian Graf <florian.graf@gmx.ch>
2017-07-02 04:35:25 +0200

    Fixed cannon muzzle rotation direction

--- EpicGameJam2017/Assets/Scripts/Cannon/MuzzleRotation.cs
+++ EpicGameJam2017/Assets/Scripts/Cannon/MuzzleRotation.cs
@@ -34,7 +34,7 @@ public class MuzzleRotation
 
 	  var direction = Input.GetAxis(Constants.HorizontalAxis + cannon.player);
     
-	  var rotation = Time.deltaTime * cannon.rotationSpeed * -direction;
+	  var rotation = Time.deltaTime * cannon.rotationSpeed * direction;
 	  currentRotation += rotation;
       
 	  if(currentRotation < maxRotationRight)
