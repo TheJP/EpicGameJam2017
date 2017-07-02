0f5d7a39421cf1248d69edc24304837bd8a9f77b
Florian Graf <florian.graf@gmx.ch>
2017-07-02 16:31:31 +0200

    Added minimap prefab (just render the camera to the minimap texture)

---
+++ EpicGameJam2017/Assets/Scripts/Minimap.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+
+public class Minimap : MonoBehaviour
+{
+    public float MinTimeAlive = 120.0f;
+    public float MaxTimeAlive = 240.0f;
+    public float MinTimeDestroyed = 120.0f;
+    public float MaxTimeDestroyed = 240.0f;
+
+    private ParticleSystem explosion;
+    private MeshRenderer meshRenderer;
+    private SpriteRenderer spriteRenderer;
+
+    // Use this for initialization
+    void Start()
+    {
+        explosion = GetComponentInChildren<ParticleSystem>();
+        meshRenderer = GetComponentInChildren<MeshRenderer>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        
+        StartCoroutine(RunExplodingMinimap());
+    }
+
+    private IEnumerator RunExplodingMinimap()
+    {
+        explosion.Stop();
+        meshRenderer.enabled = true;
+        spriteRenderer.enabled = true;
+
+        yield return new WaitForSeconds(Random.Range(MinTimeAlive, MaxTimeAlive));
+
+        explosion.Play();
+        meshRenderer.enabled = false;
+        spriteRenderer.enabled = false;
+
+        yield return new WaitForSeconds(Random.Range(MinTimeDestroyed, MaxTimeDestroyed));
+
+        StartCoroutine(RunExplodingMinimap());
+    }
+}
