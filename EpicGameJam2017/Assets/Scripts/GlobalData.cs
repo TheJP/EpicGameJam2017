52109c79c7b19fe5f696ba329921578de1e2985e
TheJP <jp_55555@hotmail.com>
2017-07-02 15:55:56 +0200

    Made projec buildable

--- EpicGameJam2017/Assets/Scripts/GlobalData.cs
+++ EpicGameJam2017/Assets/Scripts/GlobalData.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-
 using UnityEngine;
-using UnityEditor;
 using System.Collections;
 using System.Collections.Generic;
 
--- EpicGameJam2017/Assets/Scripts/Hexagon/HexagonCell.cs
+++ EpicGameJam2017/Assets/Scripts/Hexagon/HexagonCell.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using NUnit.Framework.Constraints;
 using UnityEngine.Assertions;
 
 [RequireComponent(typeof(MeshRenderer), typeof(MeshFilter))]
